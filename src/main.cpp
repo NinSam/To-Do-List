8136198d2e55d57f703988bcc102856a0de094d8
NinSam <ninsamgd@gmail.com>
2024-10-12 18:29:29 -0700

    first commit

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 using namespace geode::prelude;
 
 class $modify(MyNotepadLayer, EditorUI) {
-	
+
 	struct Fields{
 		CCMenuItemSpriteExtra* notepadbutton;
 	};
@@ -15,8 +15,8 @@ class $modify(MyNotepadLayer, EditorUI) {
 
 		return false;
 
-		if (Mod::get()->getSettingValue<int64_t>("string-setting-example") == 0){
-			CCSprite* spr = CCSprite::create("notepad_default.png"_spr);
+		if ((Mod::get()->getSettingValue<int64_t>("string-setting-example") == 0)){
+			CCSprite* spr = CCSprite::create("notepad.png"_spr);
 			m_fields->notepadbutton = CCMenuItemSpriteExtra::create(spr, this, menu_selector(MyNotepadLayer::Onnotepadbutton));
 		}
 		else if (Mod::get()->getSettingValue<int64_t>("string-setting-example") == 1){
